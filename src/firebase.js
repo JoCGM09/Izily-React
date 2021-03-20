15303ed8ab6d4254064c53200e8dfdf9d0452cb1
Jose Guerra <jose.guerra.m@uni.pe>
2021-03-20 15:31:53 -0500

    variables de entorno agregadas

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDe94zOvsOKBCHOlnEcEV7Tl0bbZOwNkbs",
-  authDomain: "izily-test.firebaseapp.com",
-  projectId: "izily-test",
-  storageBucket: "izily-test.appspot.com",
-  messagingSenderId: "1042871062612",
-  appId: "1:1042871062612:web:bfe66427e1f3684d605dc1",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const fb = firebase.initializeApp(firebaseConfig);
